5ad46b9ae10371d7d949b1e8cc69cc4d38250476
Tanner Gladson <tannergladson@gmail.com>
2024-01-09 21:01:40 -0500

    Pointer to a pointer (which points to the start of an array), not just a pointer to a single entry

--- src/xbee/xbee_device.c
+++ src/xbee/xbee_device.c
@@ -123,7 +123,7 @@ uint8_t xbee_next_frame_id( xbee_dev_t *xbee)
 */
 _xbee_device_debug
 int xbee_dev_init( xbee_dev_t *xbee, const xbee_serial_t *serport,
-                                 xbee_is_awake_fn is_awake, xbee_reset_fn reset, xbee_dispatch_table_entry_t* xbee_frame_handlers_ptr)
+                                 xbee_is_awake_fn is_awake, xbee_reset_fn reset, xbee_dispatch_table_entry_t* xbee_frame_handlers_ptr[])
 {
    int error;
 
